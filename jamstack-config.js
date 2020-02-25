c3db1a99589a9602191c7a8c5df53303261ad6cb
Daniel Kapper <dkapper01@gmail.com>
2020-02-25 03:26:33 -0500

    first commit

--- jamstack-config.js
+++ jamstack-config.js
@@ -2,15 +2,15 @@
 
 const awsmobile = {
     "aws_project_region": "us-east-1",
-    "aws_cognito_identity_pool_id": "us-east-1:ab27a337-432c-448a-a7f8-3162b6626df6",
+    "aws_cognito_identity_pool_id": "us-east-1:0953ca12-5ff2-4865-8941-f167ca903893",
     "aws_cognito_region": "us-east-1",
-    "aws_user_pools_id": "us-east-1_y0zPXTHuR",
-    "aws_user_pools_web_client_id": "7pot1p8o56dsdmd21h6eecbu3g",
+    "aws_user_pools_id": "us-east-1_tjdXbx2Em",
+    "aws_user_pools_web_client_id": "4eebom9ouab0kc4b3mcd2r15dv",
     "oauth": {},
-    "aws_appsync_graphqlEndpoint": "https://gbawslaljvfifcu4zpk3a7vvly.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_graphqlEndpoint": "https://j7o3cf2wtnhvtj5mqvxmwb46ky.appsync-api.us-east-1.amazonaws.com/graphql",
     "aws_appsync_region": "us-east-1",
     "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS",
-    "aws_user_files_s3_bucket": "jamstack-cms-612f9821-c362-4f27-b180-65895eb25d3e-dev",
+    "aws_user_files_s3_bucket": "jamstack-cms-db897273-0784-4227-bc30-33ecc3c2a55e-default",
     "aws_user_files_s3_bucket_region": "us-east-1"
 };
 
--- src/pages/editpage.js
+++ src/pages/editpage.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from "react"
 import { Router } from "@reach/router"
-import EditPage from '../components/editpage'
+import EditPage from "../components/editpage"
 
 export default function Post() {
   return (
